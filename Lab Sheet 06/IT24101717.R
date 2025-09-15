5cdf2e2be34adc29e7896b7fed76ebbdfe8dd08a
IT24101717 <IT24101717@my.sliit.lk>
2025-09-15 23:44:44 +0530

    Lab_06

---
+++ Lab Sheet 06/IT24101717.R
@@ -0,0 +1,16 @@
+setwd("C:\\Users\\dinul\\OneDrive\\Desktop\\IT24101717")
+dbinom(40,44,0.92)
+pbinom(35,44,0.92,lower.tail = TRUE)
+1-pbinom(37,44,0.92,lower.tail = TRUE)
+pbinom(37,44,0.92,lower.tail = FALSE)
+pbinom(42,44,0.92,lower.tail = TRUE)-pbinom(39,44,0.92,lower.tail = TRUE)
+
+
+
+1-pbinom(46,50,0.85,lower.tail = TRUE)
+pbinom(46,50,0.85,lower.tail = FALSE)
+
+
+
+
+dpois(15,12)
