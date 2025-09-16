5d85d62da564c534c7f3cec70dbd5aec750b511d
IT24101717 <IT24101717@my.sliit.lk>
2025-09-16 23:45:27 +0530

    Lab_07

---
+++ Lab Sheet 07/IT24101717.R
@@ -0,0 +1,11 @@
+setwd("C:\\Users\\dinul\\OneDrive\\Desktop\\IT24101717")
+#1
+punif(25, min=0, max=40, lower.tail=TRUE) - punif(10, min=0, max=40, lower.tail=TRUE)
+
+#2
+pexp(2, rate=1/3, lower.tail=TRUE)
+
+#3
+1 - pnorm(130, mean=100, sd=15)
+
+qnorm(0.95, mean=100, sd=15)
